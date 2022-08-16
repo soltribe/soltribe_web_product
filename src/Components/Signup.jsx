6ffbe03a8c96e1ca52b2443b44f5d269f5bcbac2
crustydev <ademola1145@gmail.com>
2022-08-16 10:21:13 +0100

    Add functions to handle image upload and createAccount rpc call

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -4,6 +4,15 @@ import Header from './Header';
 import logo from '../Assets/solomon.png';
 import { Link } from "react-router-dom";
 import {FiUpload } from 'react-icons/fi';
+import {useConnection, useWallet} from '@solana/wallet-adapter-react';
+import * as solanaWeb3 from '@solana/web3.js';
+import idl from '../soltribe.json';
+import {ProviderContext} from '../utils/provider';
+import {
+    generateCreatorPDA,
+
+} from './pda';
+import { useBundlr, uploadFile,  } from '../utils/bundlr';
 
 const Signup = (props) => {
     const hiddenFileInput = useRef(null);
@@ -11,14 +20,64 @@ const Signup = (props) => {
     const [descriptiontext, setDescriptionText] = useState('');
     const [file, setFile] = useState();
     const [profile, setProfile] = useState(true);
+    const {initializeBundlr, uploadFile, bundlrInstance} = useBundlr();
+    const [program , provider] = useContext(ProviderContext);
+    const [CID, setCID] = useState('');
+    const [URI, setURI] = useState('');
+
+    function onFileChange(e) {
+        const file = e.target.files[0];
+        //console.log(e.target.files);
+        if (file) {
+            const image = URL.createObjectURL(file);
+            setImage(image);
+            let reader = new FileReader();
+            reader.onload = function () {
+                if(reader.result) {
+                    setFile(Buffer.from(reader.result))
+                }
+            }
+            reader.readAsArrayBuffer(file);
+        }
+        //What is this? setProfile(false);
+    }
+
     const handleClick = event => {
         hiddenFileInput.current.click();
     };
-    const handleChange = (e) => {
-        console.log(e.target.files);
-        setFile(URL.createObjectURL(e.target.files[0]));
-        setProfile(false);
+   
+    /// This uploads the file directly from the user to Arweave 
+    /// uploadFile takes another argument which is the filetype(mp3, jpeg, mp4, etc).
+    /// do you know how to retrieve that?
+
+    const handleUpload = async() => {
+        const res = await uploadFile(file);
+        console.log('res.data: ', res.data);
+        // The id of our uploaded content, the 3rd argument in the createAccount function
+        // and gets stored on the blockchain
+        setCID(`${res.data.id}`);
+        // Location of the user's file on arweave, if you can do it fast put the link under
+        // a big 'success!' icon on top to show that it was uploaded correctly.
+        setURI(`http://arweave.net/${res.data.id}`);
     }
+    
+    async function createAccount() {
+        const creator = provider.wallet.publicKey;
+        const creatorAccount = generateCreatorPDA(program, creator);
+        try {
+            await program.methods
+            .initCreator({username}, {descriptiontext}, CID)
+            .accounts({
+                creator: creator,
+                creatorAccount: creatorAccount
+            })
+            .rpc();
+        } catch(_err) {
+            console.log(_err);
+            // do something here
+        }
+    }
+
     return (
         <div className='signup_page'>
             <Header/>
